0e1813c1da13fe92954f4b1ac053b7a1dae9deaf
Joshua Howland <development@jkhowland.com>
2014-08-14 13:46:55 -0600

    Adds initial Objective-C Project

---
+++ IntroducingFuctions-Objc/IntroducingFuctions-Objc/AppDelegate.m
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.m
+//  IntroducingFuctions-Objc
+//
+//  Created by Joshua Howland on 8/14/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+            
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+    // Insert code here...
+    
+    
+    return YES;
+}
+
+// Define new functions here...
+
+
+
+@end
