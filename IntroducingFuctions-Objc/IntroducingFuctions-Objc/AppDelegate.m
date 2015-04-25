0c475fc339058e5a45802b203dab9c56331716b0
Warren Goh <warrengohkscode@gmail.com>
2015-04-25 13:54:40 -0600

    Finished Project

--- IntroducingFuctions-Objc/IntroducingFuctions-Objc/AppDelegate.m
+++ IntroducingFuctions-Objc/IntroducingFuctions-Objc/AppDelegate.m
@@ -17,13 +17,52 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
-    // Insert code here...
     
-    
+    NSString *myName = @"Warren";
+    int numberOfDays = 4;
+                        
+    [self printIntroductions: myName];
+    [self countDownTillIntroduction: numberOfDays];
+       
     return YES;
 }
 
-// Define new functions here...
+-(void) countDownTillIntroduction:(int) numberOfDays{
+    if (numberOfDays == 0){
+        NSLog (@"The time has come");
+    }   else {
+            NSLog (@"%d days left until introductions", numberOfDays);
+            int oneLessDay = numberOfDays - 1;
+            [self countDownTillIntroduction: oneLessDay];
+    }
+}
+
+                        
+-(void)printIntroductions:(NSString*)name{
+    //declare introduction variable with NSString type
+    NSString *introduction;
+                            
+    //declare japaneseIntroduction with NSString type
+    NSString *japaneseintroduction;
+// Set introduction using stringWithFormat (or format: in Swift) class method "My name is %@", myName
+                            introduction = [NSString stringWithFormat:@"My name is %@", name];
+                            
+// Set japanese introduction using stringByAppendingString instance method by appending " to moushimasu"
+japaneseintroduction = [introduction stringByAppendingString:@" to moushimasu"];
+                            
+// print introduction and japanese Introduction
+NSLog(@"%@", introduction);
+NSLog(@"%@", japaneseintroduction);
+                            
+                            
+                            //add new function called printIntroductions
+                            // The function should accept an NSString as a parameter called name
+                            //
+
+                            
+                        }
+    
+   
 
 
 
